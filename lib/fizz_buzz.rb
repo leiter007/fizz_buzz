86566a436213ff11945b4919e593dafecbb07ef3
felix_bonnier <felix_bonnier@yahoo.com>
2019-03-19 20:03:58 +0100

    updating the happy path

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,16 +1,11 @@
-
 def fizz_buzz(number)
-  if number % number != 1
-    "Please write a number"
-      elsif number % 15 == 0
-      "fizz buzz"
-      elsif number % 5 == 0
-      'buzz'
-      elsif number % 3 == 0
-      'fizz'
+    if number % 15 == 0
+    "fizz buzz"
+    elsif number % 5 == 0
+    'buzz'
+    elsif number % 3 == 0
+    'fizz'
     else  
     number
-    end
   end
 end
-
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -13,8 +13,5 @@ describe 'fizz_buzz' do
     it "returns 'fizz buzz' if number is divisible by 3 or 5" do
         expect(fizz_buzz(15)).to eq 'fizz buzz'
     end
-    it "returns 'Please write a number' if number is a String" do
-        expect(fizz_buzz("hello")).to eq 'Please write a number'
-    end
 end
 
