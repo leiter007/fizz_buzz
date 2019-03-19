c572456aca2c479bf117b32d438ac3f5d0c3ad94
felix_bonnier <felix_bonnier@yahoo.com>
2019-03-19 19:45:31 +0100

    Adding the Fizz Buzz files after finishing happy path

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,16 @@
+
+def fizz_buzz(number)
+  if number % number != 1
+    "Please write a number"
+      elsif number % 15 == 0
+      "fizz buzz"
+      elsif number % 5 == 0
+      'buzz'
+      elsif number % 3 == 0
+      'fizz'
+    else  
+    number
+    end
+  end
+end
+
---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,20 @@
+require './lib/fizz_buzz'
+
+describe 'fizz_buzz' do
+    it 'returns 1 if number is 1' do
+        expect(fizz_buzz(1)).to eq 1 
+    end
+    it "returns 'fizz' if number is divisible by 3" do
+        expect(fizz_buzz(3)).to eq 'fizz'
+      end
+    it "returns 'buzz' if number is divisible by 5" do
+        expect(fizz_buzz(5)).to eq 'buzz'
+    end
+    it "returns 'fizz buzz' if number is divisible by 3 or 5" do
+        expect(fizz_buzz(15)).to eq 'fizz buzz'
+    end
+    it "returns 'Please write a number' if number is a String" do
+        expect(fizz_buzz("hello")).to eq 'Please write a number'
+    end
+end
+
